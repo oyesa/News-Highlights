78f9f70ecfe49286b9ace001994625f97b61b083
oyesa <mercyoyesa2018@gmail.com>
2022-05-01 16:29:04 +0300

    add science html template

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
+from email import message
+from turtle import title
 from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
   """
-  Root page 
+  Root page function
   """
 
-  return render_template('index.html')
+  title = 'Home - The  best parting shot news website'
+  return render_template('index.html', title = title)
+
+@app.route('/science/<int:science_id>')
+def science(science_id):
+
+    '''
+    View science news page function that returns the science news page and its data
+    '''
+    title = 'Science news from around the world'
+    return render_template('science.html', title = title)
