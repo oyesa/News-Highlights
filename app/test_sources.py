29efbbd3681f31a7f12ffb7c27f931bc48a61902
oyesa <mercyoyesa2018@gmail.com>
2022-05-01 18:13:23 +0300

    create unittest for both classes

---
+++ app/test_sources.py
@@ -0,0 +1,13 @@
+import unittest
+from .models import Source
+
+
+class SourceTest(unittest.TestCase):
+  def setUp(self):
+    '''
+    set up method to run before every test
+    '''
+    self.new_source = Source("reuters", "Reuters")
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_source, Source))
