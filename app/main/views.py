4c90a98b011b4395c036f4ebd86c37203202b065
oyesa <mercyoyesa2018@gmail.com>
2022-05-02 12:51:33 +0300

    create health file

--- app/main/views.py
+++ app/main/views.py
@@ -1,29 +1,64 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template
+from ..request import get_source, get_articles, get_articles_from_source_selected, get_articles_depending_on_category_of_the_source
 from . import main
-from ..models import *
-from ..request import get_article_top_headlines, get_article_everything
 
 #views
 @main.route('/')
 def index():
-  """
-  view root page function
-  """
-  business_articles = get_article_top_headlines('business')
-  all_articles = get_article_everything('all')
-  return render_template('index.html', all_articles = all_articles, business_articles = business_articles)
+    '''
+    Function that returns the index page 
+    '''
+    sources = get_source()
+    #get articles from bbc-news
+    bbc_news = get_articles_from_source_selected('bbc-news', '8')
+    aljazeera = get_articles_from_source_selected('al-jazeera-english', '8')
+    cnn_home = get_articles_from_source_selected('cnn', '1')
+    bbc_news_home = get_articles_from_source_selected('bbc-news', '2')
+    cbc_news = get_articles_from_source_selected('cbc-news', '2')
+    title = 'Home - Welcome to Parting Shot News'
+    return render_template('index.html',title=title, bcc=bbc_news_home,bbc_news=bbc_news, cnn_home=cnn_home, sources=sources, cbc_news=cbc_news,aljazeera=aljazeera)
+
+
+@main.route('/news-source/articles/<source_id>')
+def articles(source_id):
+  #Getting articles based on the source id
+    articles = get_articles(source_id)
+    title = f'{source_id}'
+
+    return render_template('articles.html', title=title, articles=articles)
+
 
 @main.route('/localnews')
 def localnews():
-  localnews_articles = get_article_everything('localnews')
-  return render_template('localnews.html', localnews = localnews)
+    localnews = get_articles_depending_on_category_of_the_source('localnews')
+    title = 'Local News - News from Kenya to the world'
+    return render_template('localnews.html',title=title,localnews=localnews)
 
-@main.route('/technology')
-def technology():
-  technology_articles = get_article_top_headlines('technology')
-  return render_template('tech.html', technology = technology)
+
+@main.route('/business')
+def business():
+    business = get_articles_depending_on_category_of_the_source('business')
+    title = 'Business - Business news from around the world'
+    return render_template('business.html',title=title,business=business)
+
 
 @main.route('/worldnews')
 def worldnews():
-  worldnews_articles = get_article_everything('world')
-  return render_template('worldnews.html', worldnews = worldnews)
+    worldnews = get_articles_depending_on_category_of_the_source(
+        'worldnews')
+    title = 'World News - News from around the world'
+    return render_template('worldnews.html', title=title, worldnews=worldnews)
+
+
+@main.route('/health')
+def health():
+    health = get_articles_depending_on_category_of_the_source('health')
+    title = 'Health - News on health from all over world'
+    return render_template('health.html', title=title, health=health)
+
+
+@main.route('/technology')
+def technology():
+    technology = get_articles_depending_on_category_of_the_source('technology')
+    title = 'Technology - News on technology from around the world'
+    return render_template('tech.html',title=title, technology=technology)
